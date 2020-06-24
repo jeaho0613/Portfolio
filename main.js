1e307af9a8a8fb1e3022d7148ec35ec52f0b54f6
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-06-24 16:22:21 +0900

    JavaScritps : Home Fade Out Aniamtion

--- main.js
+++ main.js
@@ -32,6 +32,13 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+// Make home slowly fade to trasnparent as the window scrolls down
+const home = document.querySelector(".home__containar");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
